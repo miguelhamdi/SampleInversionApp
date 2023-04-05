8657bb2231eb5f9b9314f979efaa394f326a6fce
miguelhamdi <115490570+miguelhamdi@users.noreply.github.com>
2023-04-05 21:00:00 +0200

    1st: Comments & Planning

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Import Libraries"""
+
+
+"""Load Audio File"""
+
+
+"""Determine Tempo"""
+
+
+"""Split Audio into 8-Bar Clips"""
+
+
+"""Adding Each Bar to the Inverse of the Previous Bar"""
+
+
+"""Create a folder called Components"""
+
+
+"""Save Each File into Folder"""
+
